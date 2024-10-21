6a4b15290e4c3debb8de273c819c05fda3903566
florinasutanto <florinasutanto@gmail.com>
2024-10-21 14:58:16 -0400

    reload

---
+++ .vercel/output/static/_app/immutable/entry/start.DKuT2WwQ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.NhZwRM6N.js";export{t as start};
--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -9,11 +9,11 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<meta name="author" content="The Pudding" />
+	<meta name="author" content="Citizen Codex" />
 	<meta name="news_keywords" content={keywords} />
 
 	<meta property="og:title" content={title} />
-	<meta property="og:site_name" content="The Pudding" />
+	<meta property="og:site_name" content="Citizen Codex" />
 	<meta property="og:url" content={url} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="article" />
