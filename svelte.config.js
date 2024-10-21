4826fb5f5f590a1615257feb7547367ce40d63f2
florinasutanto <florinasutanto@gmail.com>
2024-10-21 11:45:25 -0400

    Update base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapterStatic from "@sveltejs/adapter-static";
 import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
+import { sveltekit } from '@sveltejs/kit/vite';
 
 const preprocess = sveltePreprocess({
 	postcss: {
@@ -11,7 +12,16 @@ const preprocess = sveltePreprocess({
 const config = {
 	preprocess,
 	kit: {
-		adapter: adapterStatic({ strict: false })
+		adapter: adapterStatic({
+			// Use the path to your repository name, or '/' for root
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			strict: false
+		}),
+		paths: {
+			base: '/bookbans' // Update this to match your repo name
+		}
 	},
 	vitePlugin: {
 		// experimental: {
